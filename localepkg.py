228f63394418b421a8b03b2e653c828c67319050
Per Øyvind Karlsen (proyvind) <peroyvind@mandriva.org>
2014-10-23 18:31:49 +0200

    fix python script

--- localepkg.py
+++ localepkg.py
@@ -15,15 +15,15 @@ Requires(post,preun):	grep"""))
         if isoname.count(":"):
             print(rpm.expandMacro("%%{rename locales-%s}" % isoname))
             isonames.remove(isoname)
-    print("""
+    print(rpm.expandMacro("""
 %%description -n locales-"""+locale+"""
 These are the base files for """+langname+""" language
 localization; you need it to correctly display
 non-ASCII """+langname+""" characters, and for proper
 alphabetical sorting, and representation of
 dates and numbers according to
-"""+langname+""" language conventions.""")
-    print("""
+"""+langname+""" language conventions."""))
+    print(rpm.expandMacro("""
 %%post -n locales-"""+locale+"""
 %%{_bindir}/locale_install.sh %s""" % string.join(isonames, " ")+"""
 
@@ -32,10 +32,10 @@ if [ "\$1" = "0" ]; then
 	%%{_bindir}/locale_uninstall.sh %s""" % string.join(isonames, " ")+"""
 fi
 
-%%files -n locales-"""+locale)
+%%files -n locales-"""+locale))
     for isoname in isonames:
-        print("""
-%optional %{_localedir}/"""+isoname+"""
-%optional %{_localedir}/"""+isoname+""".*
-%optional %{_localedir}/"""+isoname+"""@*
-%exclude %{_localedir}/*/LC_MESSAGES/libc.mo""")
+        print(rpm.expandMacro("""
+%%optional %{_localedir}/"""+isoname+"""
+%%optional %{_localedir}/"""+isoname+""".*
+%%optional %{_localedir}/"""+isoname+"""@*
+%%exclude %{_localedir}/*/LC_MESSAGES/libc.mo"""))
